b3ecb20bc223fe21047cec891f8557e7064b97f0
mallub20@htl-kaindorf.at <mallub20@htl-kaindorf.at>
2024-02-03 12:01:03 +0100

    add about me section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,47 +1,13 @@
 "use client";
-import Image from "next/image";
-import luca from "./luca.jpeg";
 import { ReactTyped } from "react-typed";
+import IntroductionSection from "@/components/IntroductionSection";
+import AboutMeSection from "@/components/AboutMeSection";
 
 export default function Home() {
   return (
-    <div>
-      <div className="mt-[200px]  justify-center gap-52 flex  items-center ">
-        <div className="">
-          <div className="space-y-4 mb-10">
-            <h1 className="text-5xl mb-8">Hey!</h1>
-            <span className="font-bold text-5xl">I'm </span>{" "}
-            <span className="text-[#3F0071] font-bold text-5xl"> Luca.</span>
-            <div>
-              <ReactTyped
-                strings={["I code.", "I solve problems.", "I build solutions."]}
-                typeSpeed={40}
-                backSpeed={50}
-                attr="placeholder"
-                loop
-              >
-                <input
-                  type="text"
-                  disabled
-                  className="text-xl placeholder:text-black"
-                />
-              </ReactTyped>
-            </div>
-          </div>
-          <div className="flex space-x-4">
-            <button className="bg-[#3F0071] text-white  px-10 py-4 rounded-md">
-              Get in touch
-            </button>
-            <button className="text-[#3F0071] bg-white px-4  border-[#3F0071] border  py-2 rounded-md">
-              Download Resume
-            </button>
-          </div>
-        </div>
-
-        <div className="relative  w-[400px] h-[400px]">
-          <Image src={luca} className="rounded-lg " alt="Luca" fill></Image>
-        </div>
-      </div>
+    <div className="">
+      <IntroductionSection />
+      <AboutMeSection />
     </div>
   );
 }
---
+++ src/components/IntroductionSection.tsx
@@ -0,0 +1,44 @@
+import { ReactTyped } from "react-typed";
+import luca from "../app/luca.jpeg";
+import Image from "next/image";
+
+export default function IntroductionSection() {
+  return (
+    <div className="mt-[200px]  justify-center gap-52 flex  items-center ">
+      <div className="">
+        <div className="space-y-4 mb-10">
+          <h1 className="text-5xl mb-8">Hey!</h1>
+          <span className="font-bold text-5xl">I'm</span>
+          <span className="text-[#3F0071] font-bold text-5xl ml-2">Luca.</span>
+          <div>
+            <ReactTyped
+              strings={["I code.", "I solve problems.", "I build solutions."]}
+              typeSpeed={40}
+              backSpeed={50}
+              attr="placeholder"
+              loop
+            >
+              <input
+                type="text"
+                disabled
+                className="text-xl placeholder:text-black"
+              />
+            </ReactTyped>
+          </div>
+        </div>
+        <div className="flex space-x-4">
+          <button className="bg-[#3F0071] text-white  px-10 py-4 rounded-md">
+            Get in touch
+          </button>
+          <button className="text-[#3F0071] bg-white px-4  border-[#3F0071] border  py-2 rounded-md">
+            Download Resume
+          </button>
+        </div>
+      </div>
+
+      <div className="relative  w-[400px] h-[400px]">
+        <Image src={luca} className="rounded-lg " alt="Luca" fill></Image>
+      </div>
+    </div>
+  );
+}
